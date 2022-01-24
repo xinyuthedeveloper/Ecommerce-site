90ffe25f29a26ad71c59c832c2e1445d27c1df33
xinyuthedeveloper <wilsonyu0626@gmail.com>
2022-01-24 15:42:14 -0800

    wrap application with product provider

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,45 +1,50 @@
-import React from 'react'
-import logo from '../assets/logo.svg'
-import { Link } from 'react-router-dom'
-import { useProductsContext } from '../context/products_context'
-import { FaTimes } from 'react-icons/fa'
-import { links } from '../utils/constants'
-import styled from 'styled-components'
-import CartButtons from './CartButtons'
-import { useUserContext } from '../context/user_context'
+import React from "react";
+import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
+import { useProductsContext } from "../context/products_context";
+import { FaTimes } from "react-icons/fa";
+import { links } from "../utils/constants";
+import styled from "styled-components";
+import CartButtons from "./CartButtons";
+import { useUserContext } from "../context/user_context";
 
 const Sidebar = () => {
-  const isOpen = false;
+  const { isSidebarOpen, closeSidebar } = useProductsContext();
+
   const navLinks = links.map(({ id, text, url }) => {
     return (
       <li key={id}>
-        <Link to={url}>
+        <Link to={url} onClick={closeSidebar}>
           {text}
         </Link>
       </li>
-    )
+    );
   });
 
   return (
     <SidebarContainer>
-      <aside className={`${isOpen ? "sidebar show-sidebar" : "sidebar"}`}>
+      <aside
+        className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}
+      >
         <div className="sidebar-header">
-          <img src={logo} className='logo' alt="comfy sloth" />
-          <button type='button' className='close-btn'>
+          <img src={logo} className="logo" alt="comfy sloth" />
+          <button type="button" className="close-btn" onClick={closeSidebar}>
             <FaTimes />
           </button>
         </div>
-        <ul className='links'>
+        <ul className="links">
           {navLinks}
           <li>
-            <Link to="/checkout">Checkout</Link>
+            <Link to="/checkout" onClick={closeSidebar}>
+              Checkout
+            </Link>
           </li>
         </ul>
         <CartButtons />
       </aside>
     </SidebarContainer>
-  )
-}
+  );
+};
 
 const SidebarContainer = styled.div`
   text-align: center;
@@ -110,6 +115,6 @@ const SidebarContainer = styled.div`
       display: none;
     }
   }
-`
+`;
 
-export default Sidebar
+export default Sidebar;
