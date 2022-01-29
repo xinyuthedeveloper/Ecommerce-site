4872c7ffee950fc8a6dedd01790fe3147b1e344d
Xin Yu <xinyu@XindeMacBook-Pro.local>
2022-01-29 15:55:40 -0800

    initiate about and checkout page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,21 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return <main>
+    <PageHero title="about" />
+    <Wrapper className='page section section-center'>
+      <img src={aboutImg} alt="image of desk" />
+      <article>
+        <div className="title">
+          <h2>Our Story</h2>
+          <div className="underline"></div>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In iure repellat fugiat omnis voluptatem quisquam laboriosam, unde illo recusandae ducimus nostrum, praesentium iusto iste tempore ipsa ad veritatis id aspernatur!
+        </p>
+      </article>
+    </Wrapper>
+  </main>
 }
 
 const Wrapper = styled.section`
