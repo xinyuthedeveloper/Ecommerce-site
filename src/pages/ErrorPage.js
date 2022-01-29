53e3d41c66ca0e82801e9db1817323a129c4c868
Xin Yu <xinyu@XindeMacBook-Pro.local>
2022-01-29 15:37:39 -0800

    setup error page and react dependencies

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import { GiWrappedHeart } from 'react-icons/gi'
 const ErrorPage = () => {
-  return <h4>error page</h4>
+  return <Wrapper className='page-100'>
+    <section>
+      <h1>404</h1>
+      <h3>Sorry, the page you are trying to access cannot be found.</h3>
+      <Link to="/" className="btn">Back to home</Link>
+    </section>
+  </Wrapper>
 }
 
 const Wrapper = styled.main`
