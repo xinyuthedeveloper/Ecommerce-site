1986792e71c77ec567a9eac7ac10b4a76812104c
xinyuthedeveloper <wilsonyu0626@gmail.com>
2022-02-06 11:43:53 -0800

    contact and services components for home page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,22 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem et, unde, voluptatem eius magnam quaerat hic, saepe nostrum facilis dignissimos sequi ex repellat ducimus quo quibusdam quos? Optio, dolores voluptas.
+          </p>
+          <form className='content-form'>
+            <input type="email" className='form-input' placeholder='enter email'/>
+            <button type='submit' className='submit-btn'>Subscribe</button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
--- src/components/Services.js
+++ src/components/Services.js
@@ -3,7 +3,30 @@ import styled from 'styled-components'
 import { services } from '../utils/constants'
 
 const Services = () => {
-  return <h4>services </h4>
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <article className='header'>
+          <h3>custom firmotire built only for you</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, laudantium corrupti vel dicta totam blanditiis fuga tenetur accusantium quod dolorum facere delectus dignissimos nam aliquam exercitationem quae hic inventore natus.
+          </p>
+        </article>
+        <div className="services-center">
+          {services.map((service) => {
+            const {id, icon, title, text} = service;
+            return (
+              <article key={id} className='service'>
+                <span className='icon'>{icon}</span>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
+            )
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
